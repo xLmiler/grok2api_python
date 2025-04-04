2efd24eca5c528ced62d41c1b69fd19f292229ce
George Tse <65408965+GeorgeXie2333@users.noreply.github.com>
2025-04-04 11:15:48 +0800

    更新最新的模型限额

--- app.py
+++ app.py
@@ -159,15 +159,15 @@ class AuthTokenManager:
                 "ExpirationTime": 1 * 60 * 60 * 1000  # 1小时
             },
             "grok-3": {
-                "RequestFrequency": 20,
+                "RequestFrequency": 12,
                 "ExpirationTime": 2 * 60 * 60 * 1000  # 2小时
             },
             "grok-3-deepsearch": {
-                "RequestFrequency": 10,
+                "RequestFrequency": 5,
                 "ExpirationTime": 24 * 60 * 60 * 1000  # 24小时
             },
             "grok-3-deepersearch": {
-                "RequestFrequency": 3,
+                "RequestFrequency": 2,
                 "ExpirationTime": 24 * 60 * 60 * 1000  # 24小时
             },
             "grok-3-reasoning": {
